d01ee00efb318e68df2e70b82127291f1ecbc245
John Muiruri <john.muiruri@stl-horizon.com>
2022-02-10 15:38:39 +0300

    codes apis

--- backend/app/Http/Controllers/CodesController.php
+++ backend/app/Http/Controllers/CodesController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CodeDetails;
 use App\Models\Codes;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class CodesController extends Controller
 {
@@ -41,7 +44,87 @@ class CodesController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $rules = [
+
+                'fr_id' => 'required|integer',
+                'code' => 'required|string',
+                'serial_number' => 'required|string',
+                'description' => 'required|string',
+                'color_code' => 'sometimes|string',
+                'code_details_parent_id' => 'sometimes|integer',
+                'code_details' => 'sometimes|string'
+            ];
+            $messages = [
+                'fr_id.required' => 'Please choose a frequency',
+                'fr_id.integer' => 'The frequency id should be a number/integer',
+                'code.required' => 'Please enter a code name',
+                'code.string' => 'The color code should be in characters',
+                'serial_number.required' => 'Please enter serial number',
+                'serial_number.string' => 'The color code should be in characters',
+                'description,required' => 'Please enter a description',
+                'description.string' => 'The color code should be in characters',
+                'color_code.string' => 'The color code should be in characters',
+            ];
+
+            $validator = Validator::make($request->all(), $rules, $messages);
+
+            if ($validator->fails()) {
+                $data = [
+                    'success' => false,
+                    'message' => $validator->errors()->first()
+                ];
+                return response()->json($data, 422);
+            }
+
+            DB::beginTransaction();
+            $model = new Codes();
+
+            $model->fr_id = $request->fr_id;
+            $model->code = $request->code;
+            $model->serial_number = $request->serial_number;
+            $model->description = $request->description;
+            $model->color_code = $request->code ?? null;
+
+            $model->save();
+
+            if ($model) {
+
+                //add code details
+                $assignCode = $request->code_details ? $this->addCodeDetails($request, $model->id) : true;
+
+                // $model->org_codes()->save($model, ['cds_id' => $request->code, 'start_date' =>now()->toDateTimeString()]);
+                if ($assignCode) {
+
+                    DB::commit();
+                    $data = [
+                        'success' => true,
+                        'message' => 'Code created succesfully'
+                    ];
+
+
+                    return response()->json($data, 201);
+                } else {
+                    DB::rollBack();
+
+                    $data = [
+                        'success' => false,
+                        'message' => 'An error occured while adding code details'
+                    ];
+
+
+                    return response()->json($data, 500);
+                }
+            }
+        } catch (\Throwable $th) {
+            throw $th;
+
+            $data = [
+                'success' => false,
+                'message' => 'An error occured while creating code'
+            ];
+            return response()->json($data, 500);
+        }
     }
 
     /**
@@ -78,9 +161,89 @@ class CodesController extends Controller
      * @param  \App\Models\Codes  $codes
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Codes $codes)
+    public function update(Request $request, $code)
     {
-        //
+        try {
+            $rules = [
+
+                'fr_id' => 'required|integer',
+                'code' => 'required|string',
+                'serial_number' => 'required|string',
+                'description' => 'required|string',
+                'color_code' => 'sometimes|string',
+                'code_details_parent_id' => 'sometimes|integer',
+                'code_details' => 'sometimes|string'
+            ];
+            $messages = [
+                'fr_id.required' => 'Please choose a frequency',
+                'fr_id.integer' => 'The frequency id should be a number/integer',
+                'code.required' => 'Please enter a code name',
+                'code.string' => 'The color code should be in characters',
+                'serial_number.required' => 'Please enter serial number',
+                'serial_number.string' => 'The color code should be in characters',
+                'description,required' => 'Please enter a description',
+                'description.string' => 'The color code should be in characters',
+                'color_code.string' => 'The color code should be in characters',
+            ];
+
+            $validator = Validator::make($request->all(), $rules, $messages);
+
+            if ($validator->fails()) {
+                $data = [
+                    'success' => false,
+                    'message' => $validator->errors()->first()
+                ];
+                return response()->json($data, 422);
+            }
+
+            DB::beginTransaction();
+            $model = Codes::find($code);
+
+            $model->fr_id = $request->fr_id;
+            $model->code = $request->code;
+            $model->serial_number = $request->serial_number;
+            $model->description = $request->description;
+            $model->color_code = $request->color_code ?? null;
+
+            $model->save();
+
+            if ($model) {
+
+                //add code details
+                $assignCode = $request->code_details ? $this->updateCodeDetails($request, $model->id) : true;
+
+                // $model->org_codes()->save($model, ['cds_id' => $request->code, 'start_date' =>now()->toDateTimeString()]);
+                if ($assignCode) {
+
+                    DB::commit();
+                    $data = [
+                        'success' => true,
+                        'message' => 'Code created succesfully'
+                    ];
+
+
+                    return response()->json($data, 201);
+                } else {
+                    DB::rollBack();
+
+                    $data = [
+                        'success' => false,
+                        'message' => 'An error occured while adding code details'
+                    ];
+
+
+                    return response()->json($data, 500);
+                }
+            }
+        } catch (\Throwable $th) {
+            throw $th;
+
+            $data = [
+                'success' => false,
+                'message' => 'An error occured while creating code'
+            ];
+            return response()->json($data, 500);
+        }
     }
 
     /**
@@ -89,8 +252,64 @@ class CodesController extends Controller
      * @param  \App\Models\Codes  $codes
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Codes $codes)
+    public function destroy(Codes $code)
     {
-        //
+        try {
+            Codes::destroy($code);
+            $data = [
+                'success' => true,
+                'message' => 'Code deleted succesfully'
+            ];
+            return response()->json($data, 200);
+        } catch (\Throwable $th) {
+            logger($th);
+            $data = [
+                'success' => false,
+                'message' => 'An error occured while deleting code'
+            ];
+            return response()->json($data, 500);
+        }
+    }
+
+    /**
+     * Add code details
+     */
+    public function addCodeDetails(Request $request, $code)
+    {
+        try {
+            $model = new CodeDetails();
+            $model->cds_id = $code;
+            $model->parent_id = $request->code_details_parent_id ?? null;
+            $model->description = $request->code_details;
+
+            $model->save();
+
+            return true;
+        } catch (\Throwable $th) {
+            logger($th);
+
+            return false;
+        }
+    }
+
+    /**
+     * Add code details
+     */
+    public function updateCodeDetails(Request $request, $code, $code_details)
+    {
+        try {
+            $model = CodeDetails::find($code_details);
+            $model->cds_id = $code;
+            $model->parent_id = $request->code_details_parent_id ?? null;
+            $model->description = $request->code_details;
+
+            $model->save();
+
+            return true;
+        } catch (\Throwable $th) {
+            logger($th);
+
+            return false;
+        }
     }
 }
